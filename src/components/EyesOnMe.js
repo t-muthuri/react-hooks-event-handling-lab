14958c719a737cbd9417096371dbbdcddd27b835
Tressie Muthuri <tressiemuthuri@gmail.com>
2022-09-26 16:00:46 +0300

    Completed assignment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,33 @@
 // Code EyesOnMe Component Here
+import React, {useState} from "react";
+
+    function Eyesonme(){
+        const [eyesonme, setEyesonme]= useState(true)
+        console.log ("Good!");
+
+        function handleFocus(){
+            setEyesonme(eyesonme)
+            console.log("Good!")
+        }
+        function handleBlurs(){
+            setEyesonme(!eyesonme)
+            console.log("Hey! Eyes on me")
+        }
+    
+    return(
+        <div>
+            <button onClick={handleFocus}>{eyesonme ? "Good!" : "Hey! Eyes on me!"}</button>
+            <button onClick={handleBlurs}>{!eyesonme ? "Good!" : "Hey! Eyes on me!"}</button>
+        </div>
+    )
+
+}
+
+export default Eyesonme;
+
+//Deliverables
+// In the components/EyesOnMe.js file, create a EyesOnMe React component.
+// In that component, render a button with the text 'Eyes on me'.
+// On that button, add event handlers that listen for the focus and blur events.
+// When the focus event fires, use console.log to print out the text 'Good!'.
+// When the blur event fires, use console.log to print out the text 'Hey! Eyes on me!'.
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,33 @@
+import React, {useState} from "react";
+
 // Code Keypad Component Here
 
 function Keypad (){
+    const[password, setPassword]=useState("")
+
+    function handleSubmit(e){
+        e.preventDefault();
+        const keypadData={
+            password
+        }
+        console.log(keypadData)
+        console.log ("entering password...");
+    }
     return (
-        <div></div>
+        <div>
+        <form onBlur={handleSubmit}>
+        <input
+        type="password"
+        onBlur={(e)=>setPassword(e.target.value)}></input>
+        </form>
+        </div>
     )
 }
-
 export default Keypad;
+
+//Deliverables
+//use an <input type="password" /> field to capture input
+//In the components/Keypad.js file, create a Keypad React component.
+//In that component, render an input with the right type.
+//On that input, add an event handler that listens for the change event.
+//When that event fires, use console.log to print out the text 'Entering password...'
